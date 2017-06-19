3275c8841f2b07d4a0e9e3684034757d6e4552c6
LilGast8 <contact@gastonbouchayer.com>
2017-06-19 17:31:16 +0200

    💅🏻 Adjust loading asset stuff

--- dev/assets/js/app/desktop/models/Assets.js
+++ dev/assets/js/app/desktop/models/Assets.js
@@ -29,71 +29,15 @@ HLG.Models.Assets = ( function( window ) {
 			/* others */
 			
 			/* temp */
-			HLG.Path.URL.img + 'temp/home.jpg',
-			HLG.Path.URL.img + 'temp/404.jpg',
+			// HLG.Path.URL.img + 'temp/home.jpg',
+			// HLG.Path.URL.img + 'temp/404.jpg',
 		];
 		
 		
 		this.aJson = {
-			global: HLG.Path.URL.json + 'test-global.json',
-			home: HLG.Path.URL.json + 'test-home.json'
+			// global: HLG.Path.URL.json + 'test-global.json',
+			// home: HLG.Path.URL.json + 'test-home.json'
 		};
-		
-		
-		// this.aImg = {
-		// 	'global': [
-		// 		/* bgs */
-				
-		// 		/* btns */
-				
-		// 		/* icons */
-				
-		// 		/* logos */
-				
-		// 		/* others */
-		// 	],
-			
-		// 	'error-404': [
-		// 		/* temp */
-		// 		HLG.Path.URL.img + 'temp/404.jpg',
-		// 	],
-			
-		// 	'not-available': [
-		// 		/* temp */
-		// 		HLG.Path.URL.img + 'temp/not-available.gif',
-		// 	],
-			
-		// 	'home': [
-		// 		/* temp */
-		// 		HLG.Path.URL.img + 'temp/home.jpg',
-		// 	],
-			
-		// 	'about': [
-		// 		/* temp */
-		// 		HLG.Path.URL.img + 'temp/about-1.jpg',
-		// 		HLG.Path.URL.img + 'temp/about-2.jpg',
-		// 	],
-			
-		// 	'projects': [
-		// 		/* temp */
-		// 		HLG.Path.URL.img + 'temp/projects.jpg',
-		// 	]
-		// };
-		
-		
-		// this.aJson = {
-		// 	'global': {
-		// 		global: HLG.Path.URL.json + 'test-global.json'
-		// 	},
-			
-		// 	'home': {
-		// 		home: HLG.Path.URL.json + 'test-home.json'
-		// 	},
-			
-		// 	'projects': {
-		// 		projects: HLG.Path.URL.json + 'test-projects.json'
-		// 	}
-		// };
 	};
 	
 	
