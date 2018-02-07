a48617cb959f58e32ccc2b332ed81cc71f283426
LilGast8 <contact@gastonbouchayer.com>
2018-02-07 17:32:27 +0100

    🔧 Fix indentation

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,50 +1,52 @@
-var gulp = require('gulp');
-var path = require('path');
+var gulp			= require( 'gulp' );
+var path			= require( 'path' );
 
-var options = require('../utils/options');
-var cssSupports = require('../utils/css-supports');
-var paths = require('../utils/paths');
+var options			= require( '../utils/options' );
+var cssSupports		= require( '../utils/css-supports' );
+var paths			= require( '../utils/paths' );
 
-var plumber = require('gulp-plumber');
-var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var notify = require('gulp-notify');
-var rename = require('gulp-rename');
-var bs = require('browser-sync');
+var plumber			= require( 'gulp-plumber' );
+var gutil			= require( 'gulp-util' );
+var sass			= require( 'gulp-sass' );
+var sourcemaps		= require( 'gulp-sourcemaps' );
+var autoprefixer	= require( 'gulp-autoprefixer' );
+var notify			= require( 'gulp-notify' );
+var rename			= require( 'gulp-rename' );
+var bs				= require( 'browser-sync' );
 
-gulp.task('sass', ['sass:dev'], function () {
+gulp.task( 'sass', [ 'sass:dev' ], function () {
 
-	if (options.task == 'sass')
-		gulp.start('move');
+	if ( options.task == 'sass' )
+		gulp.start( 'move' );
 
-});
+} );
 
 
-gulp.task('sass:dev', ['delete'], function () {
-
-	if (options.cssSrcPath === null)
+gulp.task( 'sass:dev', [ 'delete' ], function () {
+	
+	if ( options.cssSrcPath === null )
 		options.cssSrcPath = paths.env.dev + paths.assets.css.app.dir + '*.scss';
-
-	var reload = bs.get("browsersync");
-	return gulp.src(options.cssSrcPath)
-		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(sass.sync({
-			outputStyle: 'compressed',
-			precision: 3,
-			emitCompileError: true
-		}).on('error', function (error) {
-			var file = error.relativePath.substr(error.relativePath.indexOf('/assets/') + 8);
-			var msg = 'CSS error: ' + file + ' on line ' + error.line + ', column ' + error.column;
-			notify().write(msg);
-			console.log(gutil.colors.red(error.message));
-		}))
-		.pipe(autoprefixer(cssSupports))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest(paths.env.dev + paths.assets.css.dir))
-		.pipe(reload.stream());
-
-});
+	
+	
+	var reload = bs.get( 'browsersync' );
+	
+	return gulp.src( options.cssSrcPath )
+			.pipe( plumber() )
+			.pipe( sourcemaps.init() )
+			.pipe( sass.sync( {
+				outputStyle:		'compressed',
+				precision:			3,
+				emitCompileError:	true
+			} ).on( 'error', function ( error ) {
+				var file	= error.relativePath.substr( error.relativePath.indexOf( '/assets/' ) + 8 );
+				var msg		= 'CSS error: ' + file + ' on line ' + error.line + ', column ' + error.column;
+				notify().write( msg );
+				console.log( gutil.colors.red( error.message ) );
+			}))
+			.pipe( autoprefixer( cssSupports ) )
+			.pipe( rename( { suffix: '.min' } ) )
+			.pipe( sourcemaps.write( './maps' ) )
+			.pipe( gulp.dest( paths.env.dev + paths.assets.css.dir ) )
+			.pipe( reload.stream() );
+	
+} );
