c9cc2ca2192a000cf373f9941ae919ded409ee7e
LilGast8 <contact@gastonbouchayer.com>
2018-02-28 10:47:27 +0100

    ⏳ Remove Browsersync & add Livereload

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -12,41 +12,39 @@ var sourcemaps		= require( 'gulp-sourcemaps' );
 var autoprefixer	= require( 'gulp-autoprefixer' );
 var notify			= require( 'gulp-notify' );
 var rename			= require( 'gulp-rename' );
-var bs				= require( 'browser-sync' );
 
-gulp.task( 'sass', [ 'sass:dev' ], function () {
 
+
+gulp.task( 'sass', [ 'sass:dev' ], function() {
+	
 	if ( options.task == 'sass' )
 		gulp.start( 'move' );
+	
+});
 
-} );
 
-
-gulp.task( 'sass:dev', [ 'delete' ], function () {
+gulp.task( 'sass:dev', [ 'delete' ], function() {
 	
 	if ( options.cssSrcPath === null )
-		options.cssSrcPath = paths.env.dev + paths.assets.css.app.dir + '*.scss';
+		options.cssSrcPath = paths.env.dev + paths.assets.css.app.allSCSS;
 	
-	
-	var reload = bs.get( 'browsersync' );
 	
 	return gulp.src( options.cssSrcPath )
-			.pipe( plumber() )
-			.pipe( sourcemaps.init() )
-			.pipe( sass.sync( {
-				outputStyle:		'compressed',
-				precision:			3,
-				emitCompileError:	true
-			} ).on( 'error', function ( error ) {
-				var file	= error.relativePath.substr( error.relativePath.indexOf( '/assets/' ) + 8 );
-				var msg		= 'CSS error: ' + file + ' on line ' + error.line + ', column ' + error.column;
-				notify().write( msg );
-				console.log( gutil.colors.red( error.message ) );
-			}))
-			.pipe( autoprefixer( cssSupports ) )
-			.pipe( rename( { suffix: '.min' } ) )
-			.pipe( sourcemaps.write( './maps' ) )
-			.pipe( gulp.dest( paths.env.dev + paths.assets.css.dir ) )
-			.pipe( reload.stream() );
+		.pipe( plumber() )
+		.pipe( sourcemaps.init() )
+		.pipe( sass.sync( {
+			outputStyle:		'compressed',
+			precision:			3,
+			emitCompileError:	true
+		} ).on( 'error', function( error ) {
+			var file	= error.relativePath.substr( error.relativePath.indexOf( '/assets/' ) + 8 );
+			var msg		= 'CSS error: ' + file + ' on line ' + error.line + ', column ' + error.column;
+			notify().write( msg );
+			console.log( gutil.colors.red( error.message ) );
+		} ) )
+		.pipe( autoprefixer( cssSupports ) )
+		.pipe( rename( { suffix : '.min' } ) )
+		.pipe( sourcemaps.write( './maps' ) )
+		.pipe( gulp.dest( paths.env.dev + paths.assets.css.dir ) );
 	
-} );
+});
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,20 +1,17 @@
-var gulp	= require( 'gulp' );
-var path	= require( 'path' );
+var gulp		= require( 'gulp' );
+var path		= require( 'path' );
 
-var options	= require( '../utils/options' );
-var paths	= require( '../utils/paths' );
-var bs		= require( 'browser-sync' ).create( 'browsersync' );
-var config	= require( '../../dev/configs/config.json' );
+var options		= require( '../utils/options' );
+var paths		= require( '../utils/paths' );
 
-bs.init( {
-	notify:	false,
-	port:	8080,
-	open:	false,
-	proxy:	config.ENV.base_url
-} );
+var livereload	= require( 'gulp-livereload' );
 
 
-gulp.task( 'watch', function () {
+
+gulp.task( 'watch', function() {
+	
+	livereload.listen();
+	
 	
 	/* Tasks management */
 	gulp.watch( [
@@ -31,19 +28,19 @@ gulp.task( 'watch', function () {
 		/* Config */
 		paths.env.dev + paths.configs.allFiles
 		
-	], function ( e ) {
+	], function( e ) {
 		
 		var ext, desktop, mobile, shared, /*favicons,*/ configs;
 		var taskname = null;
 		
 		options.filePath	= e.path;
 		ext					= path.extname( options.filePath );
-		options.fileName	= path.basename( options.filePath );
+		options.fileName	= path.basename( options.filePath )
 		
-		desktop	= options.filePath.indexOf( 'desktop' ) > -1 ? true : false;
-		mobile	= options.filePath.indexOf( 'mobile' ) > -1 ? true : false;
-		shared	= options.filePath.indexOf( 'shared' ) > -1 ? true : false;
-		configs	= options.filePath.indexOf( 'configs/' ) > -1 ? true : false;
+		desktop		= options.filePath.indexOf( 'desktop' ) > -1 ? true : false;
+		mobile		= options.filePath.indexOf( 'mobile' ) > -1 ? true : false;
+		shared		= options.filePath.indexOf( 'shared' ) > -1 ? true : false;
+		configs		= options.filePath.indexOf( 'configs/' ) > -1 ? true : false;
 		
 		// options.devicePath: used for SASS error notification
 		if ( desktop )
@@ -75,8 +72,7 @@ gulp.task( 'watch', function () {
 		}
 		
 		/* JS */
-		/*else*/
-		if ( ext == '.js' ) {
+		/*else*/ if ( ext == '.js' ) {
 			taskname = 'js';
 			
 			if ( desktop )
@@ -101,22 +97,24 @@ gulp.task( 'watch', function () {
 		else if ( ext == '.svg' ) {
 			taskname = 'svg';
 			
-			options.svgSrcPath = [paths.env.dev + paths.assets.svg.allFiles];
+			options.svgSrcPath = [ paths.env.dev + paths.assets.svg.allFiles ];
 		}
 		
 		
-		if ( taskname ) {
+		if ( taskname )
 			gulp.start( taskname );
-		}
 		
-	});
+	} );
 	
 	
-	/* Reload */
+	/* Livereload */
 	gulp.watch( [
 		
 		/* htaccess */
 		paths.env.dev + paths.htaccess,
+		
+		/* CSS */
+		paths.env.dev + paths.assets.css.minAllFiles,
 		
 		/* JS */
 		paths.env.dev + paths.assets.js.allFiles,
@@ -135,7 +133,6 @@ gulp.task( 'watch', function () {
 		paths.env.dev + paths.server.allFiles,
 		'!' + paths.env.dev + paths.server.vendor.allFiles
 		
-	] ).on( 'change', bs.reload );
-
-
+	] ).on( 'change', livereload.changed );
+	
 } );
